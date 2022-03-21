82de34936b204fdf7ff85b227b2ca6d1458913ff
samEmi <zcabose@ucl.ac.uk>
2022-03-21 11:56:15 +0000

    ask_kita hello world example

---
+++ AskKita-Hello-World-Example/AskKita.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace AskKita_Hello_World_Example
+{
+    internal class AskKita
+    {
+        private static string interpreter = @"ask_kita/ask_kita.exe";
+        private static bool askKitaRunning = false;
+        private static Process askKitaProcess;
+
+        public static bool kitaChangeState() {
+            if (!askKitaRunning)
+            {
+                startAskKita();
+            }
+            else {
+                stopAskKita();
+            }
+            return askKitaRunning;
+        }
+
+        public static void stopAskKita()
+        {
+            Debug.WriteLine("STOP KITA");
+            if (askKitaRunning)
+            {
+                askKitaRunning = false;
+                askKitaProcess.Kill();
+                askKitaProcess.Close();
+            }
+        }
+
+        private static void startAskKita()
+        {
+            Debug.WriteLine("START KITA");
+            askKitaRunning = true;
+            askKitaProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = interpreter,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                },
+                EnableRaisingEvents = true
+            };
+            askKitaProcess.ErrorDataReceived += Process_OutputDataReceived;
+            askKitaProcess.OutputDataReceived += Process_OutputDataReceived;
+
+            askKitaProcess.Start();
+            askKitaProcess.BeginErrorReadLine();
+            askKitaProcess.BeginOutputReadLine();
+        }
+
+        private static async void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string? data = e.Data;
+            if (data == null) return;
+            Debug.WriteLine(data);
+        }
+    }
+
+  
+}
---
+++ AskKita-Hello-World-Example/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace AskKita_Hello_World_Example
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Microphone_Click(object sender, RoutedEventArgs e)
+        {
+            bool kitaIsRunning = AskKita.kitaChangeState();
+
+            if (kitaIsRunning)
+            {
+                MicTextBlock.Text = "click to stop";
+            }
+            else
+            {
+
+                MicTextBlock.Text = "click to start";
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AskKita.stopAskKita();
+        }
+    }
+}
